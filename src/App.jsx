036a42135fa3f02aeeb17e6ee51de3332ebdc847
Eldira Safargalina <eldira-safargalina@mail.ru>
2025-07-20 00:55:09 +0900

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//defining the function 
+function getTitle(title){
+  return title;
+}
 
+//JS object 
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+//JS string primitive
+const title = 'React';
+
+function App() {
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello Eldira</h1>
+
+        <label htmlFor='search'>Search: </label>
+        <input id='search' type='text' />
+        <h2>
+          {welcome.greeting}
+          {welcome.title}
+        </h2>
+        <h3>Hello {getTitle('Eldira')}</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
