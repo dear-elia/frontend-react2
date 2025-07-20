bf2246b969d9cf1822d4b9f622a666f3e4e515e5
Eldira Safargalina <eldira-safargalina@mail.ru>
2025-07-20 19:34:12 +0900

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -54,8 +54,8 @@ function App() {
       <div>
         <h1>Hello Eldira</h1>
 
-        <label htmlFor='search'>Search: </label>
-        <input id='search' type='text' />
+        <Search />
+        
         <h2>
           {welcome.greeting}
           {welcome.title}
@@ -79,21 +79,6 @@ function App() {
         <ul>
           {list.map(function(item){
             return <li key={item.objectID}>{item.title}</li>;
-          })}
-        </ul>
-
-        <ul>
-          {list.map(function(item){
-            return (
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>{' '}
-                <span>{item.author}</span>{' '}
-                <span>{item.num_comments}</span>{' '}
-                <span>{item.points}</span>
-              </li>
-            );
           })}
         </ul>
 
@@ -125,8 +110,36 @@ function App() {
         {/* calculationg the sum of comments */}
         <h1>Total points: {totalPoints}</h1>
 
+        <List />
       </div>
   );
 }
 
+function List(){
+  return (
+    <ul>
+      {list.map(function(item){
+            return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>{' '}
+                <span>{item.author}</span>{' '}
+                <span>{item.num_comments}</span>{' '}
+                <span>{item.points}</span>
+              </li>
+            );
+          })}
+    </ul>
+  )
+}
+
+function Search(){
+  return (
+    <div>
+       <label htmlFor='search'>Search: </label>
+       <input id='search' type='text' />
+    </div>
+  )
+}
 export default App
